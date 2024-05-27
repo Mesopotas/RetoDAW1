e73afebfdfa2b405d63a79be7ef30073fdf14701
Nicolas <n.abuin2005@gmail.com>
2024-05-27 20:14:24 +0200

    Implementacion del boton delete de manera funcional

--- html + css/js/util/Adminstrador.js
+++ html + css/js/util/Adminstrador.js
@@ -39,7 +39,7 @@ async function AdminUser(category) {
             if (response.ok) {
                 alert('Cliente eliminado exitosamente.');
                 // Volver a cargar la lista de clientes después de eliminar uno
-                AdminUser+();
+                AdminUser();
             } else {
                 throw new Error('Error al eliminar cliente.');
             }
